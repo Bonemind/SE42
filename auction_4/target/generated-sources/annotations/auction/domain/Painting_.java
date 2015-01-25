49fcbd4c11454f90fa4d8d450e152e61a1ccbc94
Bonemind <Subhime@gmail.com>
2015-01-25 23:18:59 +0100

    Implemented exc 5

---
+++ auction_4/target/generated-sources/annotations/auction/domain/Painting_.java
@@ -0,0 +1,14 @@
+package auction.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2015-01-25T22:21:19")
+@StaticMetamodel(Painting.class)
+public class Painting_ { 
+
+    public static volatile SingularAttribute<Painting, String> painter;
+    public static volatile SingularAttribute<Painting, String> title;
+
+}
